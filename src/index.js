8cda8e8a6658bba32fca6cbe4938cb040bb32543
wfykitty <59632779+wfykitty@users.noreply.github.com>
2020-05-21 23:43:56 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,6 @@ import React from "react";
 import { render } from "react-dom";
 import JokeGenerator from "./jokeGenerator";
 
-// const App = () => (
-//     // <div style={styles}>
-//         <JokeGenerator />
-//     </div>
-// );
-
-// render(<App />, document.getElementById("root"));
-
 render(
 <React.StrictMode>
 <JokeGenerator />
